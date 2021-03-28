d05d573a01494d380e3943e78edf244e2a30dbef
Duhaime, Anthony <duhaimea@uqtr.ca>
2021-03-28 16:06:55 -0400

    Contact

---
+++ src/page_components/contact/ContactHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { StaticImage } from "gatsby-plugin-image"
+
+const ContactHeader = () => {
+
+    return (
+        <div className="bg-gray-100 flex items-center justify-evenly flex-col h-36 sm:flex-row sm:h-12 text-center border-gray-200 border-2">
+            <div className="flex"><StaticImage src="../../images/phone_icon.png" alt="Numéro de téléphone" width={28} height={28} /> <p>819-692-6788</p></div>
+            <div className="flex"><StaticImage src="../../images/mail_icon.png" alt="Numéro de téléphone" width={28} height={28} /> <p className="ml-2">asphalt911@gmail.com</p></div>
+            <div className="flex"><StaticImage src="../../images/adress_icon.png" alt="Numéro de téléphone" width={28} height={28} /> <p className="ml-2">410 Rue des Érables, Trois-Rivières, QC G8T 5H7</p></div>
+        </div>
+    );
+}
+export default ContactHeader;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,11 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import ContactHeader from "../page_components/contact/ContactHeader";
+import { StaticImage } from "gatsby-plugin-image"
 
 function ContactPage() {
+ 
   return (
     <Layout>
       <SEO
@@ -11,66 +14,80 @@ function ContactPage() {
         title="Contact"
       />
       <section>
-        <form>
-          <h2 className="mb-8 leading-loose">
-            Here is an example of a form built using the official Tailwind CSS Forms plugin.{` `}
-            <a
-              className="font-bold text-gray-700 no-underline"
-              href="https://github.com/tailwindlabs/tailwindcss-forms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read the docs
-            </a>
-            .
-          </h2>
+        <ContactHeader />
+        <div className="flex flex-col 2xl:flex-row">
+          <div className="w-full md:w-2/3 2xl:w-1/3  mt-4 p-4">
+            <form>
 
-          <label
-            className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="first-name"
-          >
-            First Name
-          </label>
+              <label
+                className="block mb-2 text-xs font-bold uppercase"
+                htmlFor="fullname"
+              >
+                Nom complet
+              </label>
 
-          <input
-            className="w-full mb-6 rounded-md border-gray-800"
-            id="first-name"
-            placeholder="Bill"
-            type="text"
-          />
+              <input
+                className="w-full mb-6 rounded-md border-gray-800"
+                id="fullname"
+                placeholder=""
+                type="text"
+              />
 
-          <label
-            className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="last-name"
-          >
-            Last Name
-          </label>
+              <label
+                className="block mb-2 text-xs font-bold uppercase"
+                htmlFor="fullname"
+              >
+                Courriel
+              </label>
 
-          <input
-            className="w-full mb-6 rounded-md border-gray-800"
-            id="last-name"
-            placeholder="Murray"
-            type="text"
-          />
+              <input
+                className="w-full mb-6 rounded-md border-gray-800"
+                id="email"
+                placeholder=""
+                type="text"
+              />
 
-          <label
-            className="block mb-2 text-xs font-bold uppercase"
-            htmlFor="message"
-          >
-            Message
-          </label>
+              <label
+                className="block mb-2 text-xs font-bold uppercase"
+                htmlFor="fullname"
+              >
+                Téléphone
+              </label>
 
-          <textarea
-            className="w-full mb-6 rounded-md border-gray-800"
-            id="message"
-            placeholder="Say something..."
-            rows="8"
-          />
+              <input
+                className="w-full mb-6 rounded-md border-gray-800"
+                id="email"
+                placeholder="Optionnel"
+                type="text"
+              />
 
-          <button className="px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
-            Submit
-          </button>
-        </form>
+
+              <label
+                className="block mb-2 text-xs font-bold uppercase"
+                htmlFor="message"
+              >
+                Message
+              </label>
+
+              <textarea
+                className="w-full mb-6 rounded-md border-gray-800"
+                id="message"
+                placeholder=""
+                rows="8"
+              />
+
+              <button className="px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
+                Envoyer
+              </button>
+            </form>
+          </div>
+          <div className="w-1/3 mt-4 p-4 2xl:w-2/3">
+            Heures d&apos;ouverture
+          </div>
+        </div>
+        <div className="w-2/3 h-36">
+          <StaticImage src="../images/large_map.png" alt="Carte" height={400} />
+        </div>
       </section>
     </Layout>
   );
